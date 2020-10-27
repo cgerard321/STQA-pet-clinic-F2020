b0a6332f41abac427e3b9e8feccbe2926102c2c3
cgerard321 <57333167+cgerard321@users.noreply.github.com>
2020-10-27 16:12:43 -0400

    feat(TEAMA-STQA-4): Added a test message (#9)
    Co-authored-by: Christine Gerard <cgerard@crcmail.net>

--- src/main/java/org/springframework/samples/petclinic/model/Pet.java
+++ src/main/java/org/springframework/samples/petclinic/model/Pet.java
@@ -46,6 +46,7 @@ import java.util.Set;
 @Table(name = "pets")
 public class Pet extends NamedEntity {
 
+    /* Test comment from cgerard */
     @Column(name = "birth_date")
     @DateTimeFormat(pattern = "yyyy/MM/dd")
     private LocalDate birthDate;

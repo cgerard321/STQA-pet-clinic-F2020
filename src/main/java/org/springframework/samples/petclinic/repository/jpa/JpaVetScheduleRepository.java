5950295c05189a15d8ffd7e618e17a1ae729bba2
GSwizzy <you@example.com>
2020-11-04 12:07:52 -0500

    Threw Exception for unimplemented Table

--- src/main/java/org/springframework/samples/petclinic/repository/jpa/JpaVetScheduleRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/jpa/JpaVetScheduleRepository.java
@@ -2,6 +2,7 @@ package org.springframework.samples.petclinic.repository.jpa;
 
 import org.springframework.samples.petclinic.model.VetSchedule;
 import org.springframework.samples.petclinic.repository.springdatajpa.VetScheduleRepository;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -12,9 +13,11 @@ public class JpaVetScheduleRepository implements VetScheduleRepository {
     @PersistenceContext
     private EntityManager em;
 
-// Gabriel
-//    @Override
-//    public Collection<VetSchedule> findAll() {
-//        return this.em.createQuery("SELECT day_available FROM vet_schedule LEFT JOIN FETCH Vet.id").getResultList();
-//    }
+ //Gabriel
+    @Override
+    public Collection<VetSchedule> findAll() {
+      //  return this.em.createQuery("SELECT day_available FROM vet_schedule LEFT JOIN FETCH Vet.id").getResultList();
+        throw new NotImplementedException();
+
+    }
 }

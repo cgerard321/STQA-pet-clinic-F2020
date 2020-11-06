33cb6db4e98b5ac3b1ddd7eede2d48add5719624
antoheb <54374821+antoheb@users.noreply.github.com>
2020-11-06 13:22:50 -0500

    feat(OWRT-STQA-72) : Add new email field (#36)
    * Implement new email for the owner
    
    * New email field added for owner record
    
    * Modify header email field of the owner td

--- src/main/java/org/springframework/samples/petclinic/model/Owner.java
+++ src/main/java/org/springframework/samples/petclinic/model/Owner.java
@@ -64,6 +64,10 @@ public class Owner extends Person {
     @Digits(fraction = 0, integer = 10)
     private String telephone;
 
+    @Column(name = "email")
+    @NotEmpty
+    private String email;
+
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets;
 
@@ -90,6 +94,14 @@ public class Owner extends Person {
 
     public void setTelephone(String telephone) {
         this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     protected Set<Pet> getPetsInternal() {
@@ -155,6 +167,7 @@ public class Owner extends Person {
             .append("address", this.address)
             .append("city", this.city)
             .append("telephone", this.telephone)
+            .append("email", this.email)
             .toString();
     }
 }

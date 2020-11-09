ac68e971fb12f3fb9457d6583eee851c00106273
ExianVII <54374520+ExianVII@users.noreply.github.com>
2020-11-08 21:09:04 -0500

    Feat/appt STQA40 - DB update (vet schedule table) (#31)
    Co-authored-by: Christine Gerard <cgerard@crcmail.net>
    Co-authored-by: Gabriel <54374532+GSwizzy@users.noreply.github.com>
    Co-authored-by: Charles Milette <charles.milette@gmail.com>

--- src/main/resources/db/hsqldb/populateDB.sql
+++ src/main/resources/db/hsqldb/populateDB.sql
@@ -51,3 +51,6 @@ INSERT INTO visits VALUES (1, 7, '2013-01-01', 'rabies shot');
 INSERT INTO visits VALUES (2, 8, '2013-01-02', 'rabies shot');
 INSERT INTO visits VALUES (3, 8, '2013-01-03', 'neutered');
 INSERT INTO visits VALUES (4, 7, '2013-01-04', 'spayed');
+
+INSERT INTO vet_schedule VALUES(1, 2,'A-102', 2,NULL);
+INSERT INTO vet_schedule VALUES(2, 2,'A-102', 0,NULL);

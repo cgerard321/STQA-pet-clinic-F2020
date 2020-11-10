20b32a33742cc8d25aa8465d7fdcbbc3bd64c30c
Alexandra Mormontoy <54374605+Colorin09@users.noreply.github.com>
2020-11-09 20:45:55 -0500

    feat(APPT-STQA-74): Added the appointment booking web page (layout) (#40)
    Co-authored-by: Gabriel <54374532+GSwizzy@users.noreply.github.com>
    Co-authored-by: Charles Milette <charles.milette@gmail.com>

--- src/main/java/org/springframework/samples/petclinic/web/AppointmentController.java
+++ src/main/java/org/springframework/samples/petclinic/web/AppointmentController.java
@@ -1,7 +1,9 @@
 package org.springframework.samples.petclinic.web;
-
-import org.springframework.samples.petclinic.model.Pet;
 import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Vets;
+import org.springframework.samples.petclinic.service.ClinicService;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Map;
@@ -9,9 +11,21 @@ import java.util.Map;
 @Controller
 public class AppointmentController {
 
-    @GetMapping(value = "/appointments")
-    public String initFindForm(Map<String, Object> model) {
-        model.put("pet", new Pet());
-        return "appointments/appointments";
+    private static final String APPOINT_FORM = "appointments/createAppointments";
+    private final ClinicService clinicService;
+
+    @Autowired
+    public AppointmentController(ClinicService clinicService) {
+        this.clinicService = clinicService;
+    }
+
+    /* Controller for the booking appointment page */
+    @GetMapping(value = "/appointments/create")
+    public String initCreationForm(Map<String, Object> vetInfo) {
+        Vets vetList = new Vets();
+        vetList.getVetList().addAll(this.clinicService.findVets());
+        vetInfo.put("vetAppoint", vetList);
+        return APPOINT_FORM;
     }
 }
+

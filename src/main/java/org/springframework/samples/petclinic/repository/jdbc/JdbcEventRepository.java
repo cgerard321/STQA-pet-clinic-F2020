5ab2cde401b77f671e1304417c43cb442cbffc17
Louis Choiniere <louischoiniere2001@gmail.com>
2020-11-20 13:42:45 -0500

    added more tests

---
+++ src/main/java/org/springframework/samples/petclinic/repository/jdbc/JdbcEventRepository.java
@@ -0,0 +1,10 @@
+package org.springframework.samples.petclinic.repository.jdbc;
+
+import java.util.HashMap;
+
+public class JdbcEventRepository {
+
+    public HashMap getEvents(int month) {
+        return null;
+    }
+}

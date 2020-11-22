5bac635dcc8725662dc558eb42d6f9637d061be3
Simon St-AndrÃ© <simonstandre@gmail.com>
2020-11-22 15:09:10 -0500

    Added test case for STQA-67
    Fixed bugged CSS for cards

---
+++ src/test/java/org/springframework/samples/petclinic/web/HallOfFameTests.java
@@ -0,0 +1,47 @@
+package org.springframework.samples.petclinic.web;
+
+import io.github.bonigarcia.seljup.SeleniumExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+@ExtendWith(SeleniumExtension.class)
+public class HallOfFameTests {
+
+    ChromeDriver driver;
+
+    // Test Class Created by Simon St-Andre
+    // This class is to test the hall of fame and some of its functionalities
+
+    public HallOfFameTests(ChromeDriver driver){
+        this.driver = driver;
+    }
+
+    // method to check that the card displays information
+    // Name of the displayed information should be the same as the one displayed in the title
+    @Test
+    void test_view_pet_info_in_ranking() throws Exception{
+        // Arrange
+        driver.get("http://localhost:8080/spring_framework_petclinic_war/");
+        driver.manage().window().maximize();
+
+        // Act
+        driver.findElementById("hallOfFame").click();
+
+        String firstPetName = driver.findElementById("HOF1Name").getText();
+        firstPetName = firstPetName.split(" ")[0];
+        WebElement hoverElement = driver.findElementByClassName("card");
+
+        String firstPetNameInfo = driver.findElementById("Name1").getText();
+
+        // Assert
+        assertThat(firstPetName, is(firstPetNameInfo));
+
+        driver.quit();
+    }
+}

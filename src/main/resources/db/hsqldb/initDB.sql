19e5c9fad5ed48b3719467bb56626a7d3d1b55ca
NikitaManta <54374717+NikitaManta@users.noreply.github.com>
2020-11-23 15:26:52 -0500

    Feat/coco stqa 69 give pet rating final (#72)
    Finally, I'm able to merge

--- src/main/resources/db/hsqldb/initDB.sql
+++ src/main/resources/db/hsqldb/initDB.sql
@@ -4,6 +4,7 @@ DROP TABLE schedules IF EXISTS;
 DROP TABLE vets IF EXISTS;
 DROP TABLE specialties IF EXISTS;
 DROP TABLE visits IF EXISTS;
+DROP TABLE ratings IF EXISTS;
 DROP TABLE pets IF EXISTS;
 DROP TABLE types IF EXISTS;
 DROP TABLE owners IF EXISTS;
@@ -105,3 +106,14 @@ CREATE TABLE vet_schedule
 );
 ALTER TABLE vet_schedule ADD CONSTRAINT fk_vet_schedules_vets FOREIGN KEY (vet_id) REFERENCES vets (id);
 ALTER TABLE vet_schedule ADD CONSTRAINT fk_vet_schedule_days FOREIGN KEY (day_id) REFERENCES schedules (id);
+
+CREATE TABLE ratings
+(
+    id          INTEGER IDENTITY PRIMARY KEY,
+    pet_id      INTEGER NOT NULL,
+    username   VARCHAR(30),
+    rating      INTEGER
+);
+ALTER TABLE ratings
+    ADD CONSTRAINT fk_ratings_pets FOREIGN KEY (pet_id) REFERENCES pets (id);
+CREATE INDEX ratings_pet_id ON ratings (pet_id);
